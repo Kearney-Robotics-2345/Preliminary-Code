3241bc8a22f214f208e4dc2c30a76a1616a69a04
antho_000 <antho_000@Anthony-Windows>
2014-11-03 22:22:17 -0600

    Initial Commit 2

--- src/universaldatatype/UniversalDatatype.java
+++ src/universaldatatype/UniversalDatatype.java
@@ -21,7 +21,7 @@ public class UniversalDatatype {
         System.out.println("Test2 using changeTypeAuto");
         miscDatatype Test2 = new miscDatatype();
         Test2.changeType("boolean");
-        System.out.println("Testing printType using Test2");
+        System.out.println("Testing printType using Test3");
         Test1.printType();
     }
     
