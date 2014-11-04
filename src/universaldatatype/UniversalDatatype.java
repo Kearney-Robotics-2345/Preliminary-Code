4d086d9d48b31d8d22034b3dd5970fdeb9b514f1
antho_000 <antho_000@Anthony-Windows>
2014-11-03 22:16:23 -0600

    Initial Commit

---
+++ src/universaldatatype/UniversalDatatype.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universaldatatype;
+
+/**
+ *
+ * @author Anthony
+ */
+public class UniversalDatatype {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Test1 using changeType...");
+        miscDatatype Test1 = new miscDatatype();
+        Test1.changeType();
+        System.out.println("Test2 using changeTypeAuto");
+        miscDatatype Test2 = new miscDatatype();
+        Test2.changeType("boolean");
+        System.out.println("Testing printType using Test2");
+        Test1.printType();
+    }
+    
+}
---
+++ src/universaldatatype/miscDatatype.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universaldatatype;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Anthony
+ */
+public class miscDatatype {
+    String datatype = "unassigned";
+    String newDatatype = "unassigned";
+    String oldDatatype = "unassigned";
+//Begin code for object "miscDatatype"
+    public boolean miscDatatype(String datatype) {
+        boolean createdSuccess = true;
+        return createdSuccess;
+    }
+    public void changeType() {
+        //Stores value of datatype in oldDatatype for future reference
+        oldDatatype = this.datatype;
+        System.out.println("Input new datatype (boolean, String, double, int, float) CASE-SENSITIVE- ");
+        Scanner inputDatatype = new Scanner(System.in);
+        newDatatype = inputDatatype.nextLine();
+        this.datatype = newDatatype;
+        System.out.println("miscDatatype converted from " + oldDatatype + " to " + this.datatype);
+        //Begin long and hopefully optimizable nest of case structures to convert data into byte arrays and back into data? OR is there an alternative?
+    }
+    //This is the same method as above but instead of user input, newDatatype is set as a parameter for automization
+     public void changeType(String newDatatype) {
+        oldDatatype = this.datatype;
+        this.datatype = newDatatype;
+        System.out.println("miscDatatype converted from " + oldDatatype + " to " + this.datatype);
+    }
+     public void printType() {
+         System.out.println("Datatype for this miscDatatype is: " + this.datatype);
+     }
+}
